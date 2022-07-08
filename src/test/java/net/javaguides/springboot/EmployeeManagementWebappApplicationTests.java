67f6083e016bfecd0c0f5e3e262e62f072102e9a
pasupuletirupesh <60146608+pasupuletirupesh@users.noreply.github.com>
2022-07-09 05:00:18 +0530

    Update EmployeeManagementWebappApplicationTests.java

--- src/test/java/net/javaguides/springboot/EmployeeManagementWebappApplicationTests.java
+++ src/test/java/net/javaguides/springboot/EmployeeManagementWebappApplicationTests.java
@@ -2,7 +2,7 @@ package net.javaguides.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Configuration
 @SpringBootTest
 class EmployeeManagementWebappApplicationTests {
 
